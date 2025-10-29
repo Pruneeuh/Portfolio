c2b255a488511bdc157b814b853b4197c7fd1dc0
Pruneeuh <prune.mamalet@etu.inp-n7.fr>
2025-10-29 09:29:18 +0000

    Update Experience page

--- mon-site/src/Experience.svelte
+++ mon-site/src/Experience.svelte
@@ -4,10 +4,10 @@
     <h2 style="margin: 0;">Intership at ANITI</h2>
     <p style="margin: 0;">I had the chance to realize my first intership at ANITI <br/>
     I have worked on the development of a position estimation algorithm (PnP) in Numpy and Pytorch for optimising the learning function of YOLO-NAS for vision-based landing. 
-    The objective was to estimate the positon of the airplaine from the image they have, in order to take in consideration that the model can do more errors, if the plane is far away from the runway <br/>
+    The objective was to estimate the positon of the airplaine from the image they have, in order to take in consideration that the model can do more errors, if the plane is far away from the runway.<br/>
     You can find my work here : <a href="https://github.com/Pruneeuh/POSEIDON" target="_blank" rel="noopener noreferrer"> POSEIDON </a>
     </p>
-    <p> In order to implement this algorithm I'v also work on a Differentiable Root Solvers for Cubic and Quartic Polynomials :   <a href="https://github.com/Pruneeuh/AutoRoot" target="_blank" rel="noopener noreferrer"> AutoRoot </a>
+    <p> In order to implement this algorithm I'v also work on a Differentiable Root Solvers for Cubic and Quartic Polynomials :   <a href="https://github.com/Pruneeuh/AutoRoot" target="_blank" rel="noopener noreferrer"> AutoRoot </a> </p>
   </div>
   <img src="https://aniti.univ-toulouse.fr/wp-content/uploads/2024/06/01-FULL-bleu.png" alt="Logo ANITI" style="height: 8em; width: auto;" />
 </div>
