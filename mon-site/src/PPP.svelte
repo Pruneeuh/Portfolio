a824b0bf719a0e156e61b42a461154e2f45596b3
Pruneeuh <prune.mamalet@etu.inp-n7.fr>
2025-11-03 20:33:33 +0000

    Add PPP presentation cadre

--- mon-site/src/PPP.svelte
+++ mon-site/src/PPP.svelte
@@ -1,8 +1,21 @@
 <script>
  import CV from '/public/CVs.pdf';
+ import PPP from '/public/PPP.pdf';
 </script>
 
 <h1>Personal Professional Project </h1>
+Here you can find my presentation : 
+<div style="display: flex; justify-content: center; margin-top: 32px; margin-bottom: 8px;">
+  <iframe
+    title="Cv"
+    src={PPP}
+    width="768"
+    height="432"
+    style="border: 1px solid #ddd;"
+    allow="autoplay"
+  ></iframe>
+</div>
+
 
 <h2 style="text-align: left;">My CV : </h2>
 <div style="display: flex; justify-content: center; margin-top: 32px; margin-bottom: 8px;">
